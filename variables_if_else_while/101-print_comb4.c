768a4fabf78436dd8161aa7b262de76db5c6ece0
tala2277 <tala2277@users.noreply.github.com>
2026-02-25 22:47:10 +0000

    Add 101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -7,29 +7,29 @@
  */
 int main(void)
 {
-        int i;
-        int j;
-        int k;
+	int i;
+	int j;
+	int k;
 
-        for (i = 0; i <= 7; i++)
-        {
-                for (j = i + 1; j <= 8; j++)
-                {
-                        for (k = j + 1; k <= 9; k++)
-                        {
-                                putchar(i + '0');
-                                putchar(j + '0');
-                                putchar(k + '0');
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-                                if (i != 7 || j != 8 || k != 9)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
