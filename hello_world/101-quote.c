374e98f9092ecb488ac06971920231e1ed0f95ea
tala2277 <tala2277@users.noreply.github.com>
2026-02-25 22:19:07 +0000

    Add 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - prints a quote to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
+}
