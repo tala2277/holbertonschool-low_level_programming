cc5a1bc215d47c5aa73ba88143476ddc74401737
tala2277 <tala2277@users.noreply.github.com>
2026-02-26 20:55:15 +0000

    Fix 100-times_table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,23 +1,41 @@
 #include "main.h"
 
+/**
+ * print_times_table - prints the n times table
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
-        int i, j, result;
+	int i, j, result;
 
-        if (n < 0 || n > 15)
-                return;
+	if (n < 0 || n > 15)
+		return;
 
-        for (i = 0; i <= n; i++)
-        {
-                for (j = 0; j <= n; j++)
-                {
-                        result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
 
-                        if (j == 0)
-                        {
-                                _putchar(result + '0');
-                        }
-                        else
-                                           [ Read 40 lines ]
-^G Help       ^O Write Out  ^W Where Is   ^K Cut        ^T Execute    ^C Location   M-U Undo
-^X Exit       ^R Read File  ^\ Replace    ^U Paste      ^J Justify    ^/ Go To Line M-E Redo
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 100)
+					_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+				if (result >= 100)
+					_putchar((result / 100) + '0');
+				if (result >= 10)
+					_putchar(((result / 10) % 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
