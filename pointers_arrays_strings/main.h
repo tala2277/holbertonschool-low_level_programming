4eaa4583adba8b053f2ae8f1cc4fdf1671a0dc05
tala2277 <tala2277@users.noreply.github.com>
2026-03-02 21:29:37 +0000

    Fix print_number

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,9 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
+void print_number(int);
 
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
 #endif
