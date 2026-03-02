e7cbc8aa48c6c00afa5f5f18568e8195d7a3c30d
tala2277 <tala2277@users.noreply.github.com>
2026-03-02 21:20:19 +0000

    Add cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to modify
+ *
+ * Return: pointer to str
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize = 1;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' ||
+		    str[i] == '{' || str[i] == '}')
+		{
+			capitalize = 1;
+		}
+		else if (capitalize && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			capitalize = 0;
+		}
+		else
+		{
+			capitalize = 0;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
