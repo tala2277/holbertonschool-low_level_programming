03bb0fb588aa836f9639e90254d1b62e7006faf5
tala2277 <tala2277@users.noreply.github.com>
2026-03-02 22:33:14 +0000

    Fix magic task

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - entry point
- *
- * Return: Always 0.
- */
 int main(void)
 {
-        int n;
-        int a[5];
-        int *p;
+    int n;
+    int a[5];
+    int *p;
 
-        a[2] = 1024;
-        p = &n;
+    a[2] = 1024;
+    p = &n;
 
-        *(p - 3) = 98;
+    printf("&n=%p\n", (void *)&n);
+    printf("&a[2]=%p\n", (void *)&a[2]);
 
-        printf("a[2] = %d\n", a[2]);
-        return (0);
+    *(p + 5) = 98;
+
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
