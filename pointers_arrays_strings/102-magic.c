afaa05dea5bb02e84f26f894f4ec7fae0003beff
tala2277 <tala2277@users.noreply.github.com>
2026-03-02 21:36:38 +0000

    Solve magic

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,1 +1,27 @@
-*(p + 5) = 98
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	p = a;
+
+	n = 98;
+	a[0] = 0;
+	a[1] = 1;
+	a[2] = 2;
+	a[3] = 3;
+	a[4] = 4;
+
+	*(p + 5) = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
