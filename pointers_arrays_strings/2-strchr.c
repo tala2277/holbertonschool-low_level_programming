c50e5bac085a67fbd9f80542089dd83af0a960c6
tala2277 <tt1341700@gmail.com>
2026-03-10 22:22:50 +0000

    Task 2 strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to find
+ *
+ * Return: pointer to first occurrence of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (0);
+}
