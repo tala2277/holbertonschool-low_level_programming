83db55d970e20c265f172e91ad3af27f95eff546
tala2277 <tt1341700@gmail.com>
2026-03-10 22:15:12 +0000

    Task 0 memset

--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 Pointers, arrays and strings
+Pointers arrays and strings in C.
+Project about pointers to pointers multidimensional arrays and string functions.
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,3 +7,10 @@ void print_number(int);
 #endif
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+#endif
