0898c3c11061d39f6f72c73b2af6fb217b887dc1
tala2277 <tala2277@gmail.com>
2026-03-30 08:17:09 +0000

    FINAL FIX: no leaks

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -4,9 +4,9 @@
 int _putchar(char c);
 
 /**
- * print_error - prints Error and exits
+ * error_exit - prints Error and exits
  */
-void print_error(void)
+void error_exit(void)
 {
 	_putchar('E');
 	_putchar('r');
@@ -39,23 +39,23 @@ int is_digit(char *s)
 	return (1);
 }
 
-void print_result(int *result, int size)
+void print_number(int *res, int len)
 {
 	int i = 0;
 
-	while (i < size && result[i] == 0)
+	while (i < len && res[i] == 0)
 		i++;
 
-	if (i == size)
+	if (i == len)
 	{
 		_putchar('0');
 		_putchar('\n');
 		return;
 	}
 
-	while (i < size)
+	while (i < len)
 	{
-		_putchar(result[i] + '0');
+		_putchar(res[i] + '0');
 		i++;
 	}
 	_putchar('\n');
@@ -63,45 +63,45 @@ void print_result(int *result, int size)
 
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int len1, len2, len, i, j, mul, carry;
-	int *result;
+	char *n1, *n2;
+	int len1, len2, len, i, j, carry, mul;
+	int *res;
 
 	if (argc != 3)
-		print_error();
+		error_exit();
 
-	num1 = argv[1];
-	num2 = argv[2];
+	n1 = argv[1];
+	n2 = argv[2];
 
-	if (!is_digit(num1) || !is_digit(num2))
-		print_error();
+	if (!is_digit(n1) || !is_digit(n2))
+		error_exit();
 
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
+	len1 = _strlen(n1);
+	len2 = _strlen(n2);
 	len = len1 + len2;
 
-	result = malloc(sizeof(int) * len);
-	if (result == NULL)
-		print_error(); /* هنا مهم */
+	res = malloc(sizeof(int) * len);
+	if (res == NULL)
+		error_exit();
 
 	for (i = 0; i < len; i++)
-		result[i] = 0;
+		res[i] = 0;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		carry = 0;
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			mul = (num1[i] - '0') * (num2[j] - '0');
-			mul += result[i + j + 1] + carry;
-			result[i + j + 1] = mul % 10;
+			mul = (n1[i] - '0') * (n2[j] - '0');
+			mul += res[i + j + 1] + carry;
+			res[i + j + 1] = mul % 10;
 			carry = mul / 10;
 		}
-		result[i] += carry;
+		res[i] += carry;
 	}
 
-	print_result(result, len);
-	free(result);
+	print_number(res, len);
+	free(res);
 
 	return (0);
 }
