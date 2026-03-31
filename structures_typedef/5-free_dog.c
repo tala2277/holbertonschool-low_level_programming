ea2a50b2842eb6f56eeeeb028035b5122a3713e2
tala2277 <tt1341700@gmail.com>
2026-03-31 08:04:10 +0000

    Task 5: free_dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
