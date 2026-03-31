2d83a13374dd21d1888a7bffd89bf86941f780ff
tala2277 <tt1341700@gmail.com>
2026-03-31 07:58:26 +0000

    fix: add betty documentation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 
+/**
+ * init_dog - initialize struct dog
+ * @d: pointer to struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != 0)
