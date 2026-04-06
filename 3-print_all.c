fb0a0ac8d24368b9d45ce496b56b223f2f741d7d
tala2277 <tt1341700@gmail.com>
2026-04-06 09:57:57 +0000

    Final fix print_all (2 if only)

--- 3-print_all.c
+++ 3-print_all.c
@@ -3,46 +3,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints a char
- * @args: va_list
- */
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - prints an int
- * @args: va_list
- */
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - prints a float
- * @args: va_list
- */
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - prints a string
- * @args: va_list
- */
-void print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-		str = "(nil)";
-	printf("%s", str);
-}
-
-/**
  * print_all - prints anything
  * @format: list of types
  */
@@ -51,13 +11,6 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0, j;
 	char *sep = "";
-
-	void (*funcs[])(va_list) = {
-		print_char,
-		print_int,
-		print_float,
-		print_string
-	};
 
 	char types[] = {'c', 'i', 'f', 's'};
 
@@ -71,7 +24,19 @@ void print_all(const char * const format, ...)
 			if (format[i] == types[j])
 			{
 				printf("%s", sep);
-				funcs[j](args);
+
+				if (j == 0)
+					printf("%c", va_arg(args, int));
+				if (j == 1)
+					printf("%d", va_arg(args, int));
+				if (j == 2)
+					printf("%f", va_arg(args, double));
+				if (j == 3)
+				{
+					char *str = va_arg(args, char *);
+					printf("%s", str ? str : "(nil)");
+				}
+
 				sep = ", ";
 			}
 			j++;
