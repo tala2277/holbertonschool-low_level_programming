36f354484b18364bcc8e319c65587aec26b3be64
tala2277 <tt1341700@gmail.com>
2026-04-06 09:39:51 +0000

    Fix NULL error in get_op_func

---
+++ 3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects correct function
+ * @s: operator
+ * Return: function pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL && s[0] != ops[i].op[0])
+		i++;
+
+	return (ops[i].f);
+}
