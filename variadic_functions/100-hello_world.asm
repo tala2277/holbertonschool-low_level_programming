5e000812dd55cd6a26f110e981fe0aee910ed3ec
tala2277 <tt1341700@gmail.com>
2026-04-06 10:16:33 +0000

    Fix 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,15 @@
+global main
+
 section .data
-	msg db "Hello, World", 10
-	len equ $ - msg
+    msg db "Hello, World", 10
 
 section .text
-	global _start
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
 
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, len
-	syscall
-
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+    xor eax, eax
+    ret
