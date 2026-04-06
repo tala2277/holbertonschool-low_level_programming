898774b9e3560184c64eb2201583169014be94a7
tala2277 <tt1341700@gmail.com>
2026-04-06 09:47:12 +0000

    Task 4: main opcodes

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	unsigned char *ptr = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", ptr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
