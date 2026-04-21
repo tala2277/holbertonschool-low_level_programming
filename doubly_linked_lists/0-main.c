4c417d20deb8847acd3af064b2852b9dddc0a5e5
tala2277 <tt1341700@gmail.com>
2026-04-21 13:06:25 +0000

    Start doubly linked lists project

---
+++ doubly_linked_lists/0-main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+int main(void)
+{
+	dlistint_t *head;
+	dlistint_t *new;
+	dlistint_t hello = {8, NULL, NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
+	n = print_dlistint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,6 @@
+# Doubly Linked Lists
+
+This project covers doubly linked lists in C.
+
+## Author
+Tala Alhudhaibi
