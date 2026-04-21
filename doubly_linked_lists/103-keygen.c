1d54ea1bbdb54f80efac62593e5d973f1bd90856
tala2277 <tt1341700@gmail.com>
2026-04-21 14:20:31 +0000

    Corrected keygen with proper logic and Betty style

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,21 +3,29 @@
 #include <time.h>
 
 /**
- * main - Generates a key for crackme5.
- * @argc: The number of arguments.
- * @argv: The arguments vector.
+ * main - Generates a key for crackme5
+ * @argc: Number of arguments
+ * @argv: Arguments vector
  *
- * Return: 0 on success, 1 on error.
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
  char key[7];
  char *lookup;
  char *username;
- int len, i, sum, prod, max, sum_sq, res_rand;
+ int len;
+ int i;
+ int sum;
+ int prod;
+ int max;
+ int sum_sq;
+ int res_rand;
 
  if (argc != 2)
+ {
   return (1);
+ }
 
  lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./";
  username = argv[1];
@@ -27,26 +35,36 @@ int main(int argc, char *argv[])
  key[0] = lookup[(len ^ 59) & 63];
  sum = 0;
  for (i = 0; i < len; i++)
+ {
   sum += username[i];
+ }
  key[1] = lookup[(sum ^ 79) & 63];
  prod = 1;
  for (i = 0; i < len; i++)
+ {
   prod *= username[i];
+ }
  key[2] = lookup[(prod ^ 85) & 63];
  max = username[0];
  for (i = 0; i < len; i++)
  {
   if (username[i] > max)
+  {
    max = username[i];
+  }
  }
  srand(max ^ 14);
  key[3] = lookup[rand() & 63];
  sum_sq = 0;
  for (i = 0; i < len; i++)
+ {
   sum_sq += (username[i] * username[i]);
+ }
  key[4] = lookup[(sum_sq ^ 239) & 63];
  for (i = 0; i < username[0]; i++)
+ {
   res_rand = rand();
+ }
  key[5] = lookup[(res_rand ^ 229) & 63];
  key[6] = '\0';
  printf("%s", key);
