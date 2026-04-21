79948d7ba5889a627f35a0255b46b4d91fff86c3
tala2277 <tt1341700@gmail.com>
2026-04-21 14:30:07 +0000

    Fix 103-keygen

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -24,16 +24,20 @@ int main(int argc, char *argv[])
 	for (len = 0; name[len]; len++)
 		;
 
+	/* key[0] */
 	key[0] = lookup[(len ^ 59) & 63];
 
+	/* key[1] */
 	for (i = 0; i < len; i++)
 		sum += name[i];
 	key[1] = lookup[(sum ^ 79) & 63];
 
+	/* key[2] */
 	for (i = 0; i < len; i++)
 		prod *= name[i];
 	key[2] = lookup[(prod ^ 85) & 63];
 
+	/* key[3] */
 	max = name[0];
 	for (i = 0; i < len; i++)
 		if (name[i] > max)
@@ -41,10 +45,12 @@ int main(int argc, char *argv[])
 	srand(max ^ 14);
 	key[3] = lookup[rand() & 63];
 
+	/* key[4] */
 	for (i = 0; i < len; i++)
 		sum_sq += name[i] * name[i];
 	key[4] = lookup[(sum_sq ^ 239) & 63];
 
+	/* key[5] */
 	for (i = 0, r = 0; i < name[0]; i++)
 		r = rand();
 	key[5] = lookup[(r ^ 229) & 63];
