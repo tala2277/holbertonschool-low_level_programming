497c55b26c93f6e00d7e553d254157e62f64a900
tala2277 <tt1341700@gmail.com>
2026-04-21 13:32:57 +0000

    Add clean keygen structure

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
 /**
  * main - handles username input
- * @argc: argument count
- * @argv: argument vector
+ * @argc: number of arguments
+ * @argv: arguments vector
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	const char *username;
-	size_t i, len;
+	char key[7];
+	int i, sum;
+	char *name;
+	char *lookup = "A-CH@n3123456789h";
 
 	if (argc != 2)
 		return (1);
 
-	username = argv[1];
-	len = strlen(username);
+	name = argv[1];
+	sum = 0;
 
-	for (i = 0; i < len; i++)
-		putchar(username[i]);
+	for (i = 0; name[i]; i++)
+		sum += name[i];
 
-	putchar('\n');
+	srand(sum ^ 0x11);
+
+	for (i = 0; i < 6; i++)
+		key[i] = lookup[rand() & 0xF];
+
+	key[6] = '\0';
+	printf("%s", key);
 
 	return (0);
 }
