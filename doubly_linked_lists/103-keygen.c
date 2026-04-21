4669d5a683396079058669bee7189202ac37c1ea
tala2277 <tt1341700@gmail.com>
2026-04-21 14:13:22 +0000

    Update 103-keygen template

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * main - handles username input
+ * main - handles program arguments
  * @argc: number of arguments
- * @argv: argument vector
+ * @argv: array of arguments
  *
- * Return: 0 on success, 1 on failure
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	const char *username;
-	size_t i;
-
 	if (argc != 2)
 		return (1);
 
-	username = argv[1];
-
-	for (i = 0; username[i] != '\0'; i++)
-		putchar(username[i]);
-
-	putchar('\n');
+	printf("%s\n", argv[1]);
 
 	return (0);
 }
