2e6f9762c7a6c070d675799daa2f4cdaf7b6444c
tala2277 <tt1341700@gmail.com>
2026-04-21 13:03:40 +0000

    Add free_list function

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -52,4 +52,12 @@ list_t *add_node(list_t **head, const char *str);
  */
 list_t *add_node_end(list_t **head, const char *str);
 
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head);
+
 #endif
