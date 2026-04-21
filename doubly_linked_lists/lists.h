7b24d75d7229f009d4404801051d01b81d557a3b
tala2277 <tt1341700@gmail.com>
2026-04-21 13:17:20 +0000

    Add insert_dnodeint_at_index function

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -77,4 +77,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
  */
 int sum_dlistint(dlistint_t *head);
 
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to pointer of the head of the list
+ * @idx: index where the new node should be added
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
 #endif
