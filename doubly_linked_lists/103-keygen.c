e3e53ca0fee843c2ba7ae086da192a473e524cd5
tala2277 <tt1341700@gmail.com>
2026-04-21 13:41:37 +0000

    Fix keygen algorithm for crackme5

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,19 +3,18 @@
 #include <string.h>
 
 /**
- * main - Generates a valid key for crackme5.
- * @argc: Number of arguments.
- * @argv: Arguments vector.
- *
- * Return: 0 on success, 1 on error.
+ * main - Keygen for crackme5
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
- unsigned int i, res;
+ unsigned int i, sum, max_val;
  size_t len;
  char *user;
  char key[7];
- char *lookup = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU641omwEhe7preirSTZ";
+ const char *lookup = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU641omwEhe7preirSTZ";
 
  if (argc != 2)
   return (1);
@@ -23,40 +22,39 @@ int main(int argc, char *argv[])
  user = argv[1];
  len = strlen(user);
 
- /* 1. Based on length */
+ /* Key index 0 */
  key[0] = lookup[(len ^ 59) & 63];
 
- /* 2. Based on sum of chars */
- for (i = 0, res = 0; i < len; i++)
-  res += user[i];
- key[1] = lookup[(res ^ 79) & 63];
+ /* Key index 1 */
+ for (i = 0, sum = 0; i < len; i++)
+  sum += user[i];
+ key[1] = lookup[(sum ^ 79) & 63];
 
- /* 3. Based on product of chars */
- for (i = 0, res = 1; i < len; i++)
-  res *= user[i];
- key[2] = lookup[(res ^ 85) & 63];
+ /* Key index 2 */
+ for (i = 0, sum = 1; i < len; i++)
+  sum *= user[i];
+ key[2] = lookup[(sum ^ 85) & 63];
 
- /* 4. Based on max char and randomness */
- res = 0;
+ /* Key index 3 */
+ max_val = user[0];
  for (i = 0; i < len; i++)
-  if ((unsigned int)user[i] > res)
-   res = (unsigned int)user[i];
- srand(res ^ 14);
+  if ((unsigned int)user[i] > max_val)
+   max_val = user[i];
+ srand(max_val ^ 14);
  key[3] = lookup[rand() & 63];
 
- /* 5. Based on sum of squares */
- res = 0;
- for (i = 0; i < len; i++)
-  res += (user[i] * user[i]);
- key[4] = lookup[(res ^ 239) & 63];
+ /* Key index 4 */
+ for (i = 0, sum = 0; i < len; i++)
+  sum += (user[i] * user[i]);
+ key[4] = lookup[(sum ^ 239) & 63];
 
- /* 6. Based on first char and loop randomness */
- res = 0;
- for (i = 0; i < (unsigned int)user[0]; i++)
-  res = rand();
- key[5] = lookup[(res ^ 229) & 63];
+ /* Key index 5 */
+ for (i = 0, sum = 0; i < (unsigned int)user[0]; i++)
+  sum = rand();
+ key[5] = lookup[(sum ^ 229) & 63];
 
  key[6] = '\0';
  printf("%s", key);
+
  return (0);
 }
