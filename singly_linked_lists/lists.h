2a54902d90f4bb15945c926af712b8293aee3d71
tala2277 <tt1341700@gmail.com>
2026-04-21 12:56:12 +0000

    Fix Betty documentation

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
@@ -10,6 +18,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h);
 
 #endif
